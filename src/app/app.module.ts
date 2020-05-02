9352d72914c9232aa8341cf41e3f9472c09d5857
Adriana Rodriguez <agrodriguezs09@gmail.com>
2020-05-02 22:30:02 +0200

    about e item pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AbautComponent } from './pages/abaut/abaut.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AbautComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
