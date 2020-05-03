9fa1594faf3d5a1c25fa68a1e3973c3129a80e2a
Adriana Rodriguez <agrodriguezs09@gmail.com>
2020-05-03 13:35:39 +0200

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AbautComponent } from './pages/abaut/abaut.component';
+import { ItemComponent } from './pages/item/item.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: PortafolioComponent},
+  {path: 'about', component: AbautComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
