30b0827ffa89e0000f8c5c26aaa80ce7801a199f
Adriana Rodriguez <agrodriguezs09@gmail.com>
2020-05-04 19:47:53 +0200

    Importar data de Google Firebase

--- src/app/pages/abaut/abaut.component.ts
+++ src/app/pages/abaut/abaut.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-abaut',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbautComponent implements OnInit {
 
-  constructor() { }
+  constructor( public servicio: InfoPaginaService  ) { }
 
   ngOnInit(): void {
   }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,15 +8,29 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any [] = [];
   cargada = false;
 
   constructor(private http: HttpClient){
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+   // leer el archivo json
+   this.http.get('assets/data/data-paginas.json')
+   .subscribe( (resp: InfoPagina) => {
+     this.cargada = true;
+     this.info = resp;
+   });
+  }
+
+  private cargarEquipo() {
     // leer el archivo json
-    this.http.get('assets/data/data-paginas.json')
-    .subscribe( (resp: InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
+    this.http.get('https://angular-html-f32d9.firebaseio.com/equipo.json')
+    .subscribe( (resp: any []) => {
+      this.equipo = resp;
       console.log(resp);
     });
-  }
+   }
 }
